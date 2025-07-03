1514250a82def97c2610a1d67a7f6522ad1d7238
Георгий Пегов <gera5_7@mail.ru>
2025-07-03 10:17:24 +0300

    update

--- src/jwt_auth/urls.py
+++ src/jwt_auth/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import RegisterTemplatesView, LoginAPIView, RefreshTokenAPIView,  LoginView
+from .views import RegisterTemplatesView, LoginTemplatesView, RefreshTokenAPIView
 
 urlpatterns = [
     path('api/auth/register/', RegisterTemplatesView.as_view(), name='register-api'),
-    path('api/auth/login/', LoginAPIView.as_view(), name='login-api'),
+    path('api/auth/login/', LoginTemplatesView.as_view(), name='login-api'),
     path('api/auth/refresh/', RefreshTokenAPIView.as_view(), name='refresh-api'),
-    # path('auth/register/', RegisterView.as_view(), name='register'),
-    path('auth/login/', LoginView.as_view(), name='login'),
 ]
--- src/jwt_auth/views.py
+++ src/jwt_auth/views.py
@@ -11,29 +11,9 @@ from jwt_auth.forms import AuthorizationForm, RefreshTokenForm
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from django.http import HttpResponse
+import time
 
-
-# class RegisterView(TemplateView):
-#     template_name = 'auth/register.html'  
-
-
-class LoginView(TemplateView):
-    template_name = 'auth/login.html'
-
-
-# class RegisterAPIView(APIView):
-#     def post(self, request):
-#         serializer = RegisterSerializer(data=request.data)
-#         if serializer.is_valid():
-#             user = serializer.save()
-#             refresh = RefreshToken.for_user(user)
-#             return Response({
-#                 "access": str(refresh.access_token),
-#                 "refresh": str(refresh)
-#             }, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class RegisterTemplatesView(TemplateView):
+class RegisterTemplatesView(TemplateView, APIView):
     template_name = 'auth/register.html'
 
     def get_context_data(self, **kwargs):
@@ -41,45 +21,50 @@ class RegisterTemplatesView(TemplateView):
         context['form'] = AuthorizationForm()
         return context
     
-    def post(self, request):
-        print('hellllo')
+    def post(self, request, *args, **kwargs):
         form = AuthorizationForm(request.POST)
-        print(form, 'hellllo')
         if form.is_valid():
-            serializer = RegisterSerializer(form)
-            if not serializer.is_valid():
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        refresh = RefreshToken(serializer)
-        return HttpResponse({
-            'access_token': refresh,
-            "refresh_token": refresh.access_token
-        })
+            serializer = RegisterSerializer(data=form.cleaned_data)
+        if serializer.is_valid():
+            user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        return Response({
+                'access_token': str(refresh.access_token),
+                'refresh_token': str(refresh),
+            }, status=status.HTTP_201_CREATED)
 
-class LoginAPIView(APIView):
-    def post(self, request):
-        serializer = LoginSerializer(data=request.data)
+class LoginTemplatesView(TemplateView, APIView):
+    template_name = 'auth/login.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = AuthorizationForm()
+        return context
+    
+    def post(self, request, *args, **kwargs):
+        form = AuthorizationForm(request.POST)
+        print(form.is_valid(), 'qwerqwerqwer')
+        if not form.is_valid():
+            return Response({'error': 'not validated'})
+        else:
+            form.is_valid()
+        serializer = LoginSerializer(data=form.cleaned_data)
         if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-        try:
-            user = AuthorizationModel.objects.get(login=serializer.validated_data['login'])
-            if not check_password(serializer.validated_data['password'], user.password):
-                return Response(
-                    {"error": "Invalid password"}, 
-                    status=status.HTTP_401_UNAUTHORIZED
-                )
-            
-            refresh = RefreshToken.for_user(user)
-            return Response({
-                "access": str(refresh.access_token),
-                "refresh": str(refresh)
-            })
-            
-        except AuthorizationModel.DoesNotExist:
+            return Response({'error': 'not validated'})
+        else:
+            serializer.is_valid()
+
+        user = AuthorizationModel.objects.get(login=serializer.validated_data['login'])
+        if not check_password(serializer.validated_data['password'], user.password):
             return Response(
-                {"error": "User not found"}, 
-                status=status.HTTP_404_NOT_FOUND
+                {"error": "Invalid password"}, 
+                status=status.HTTP_401_UNAUTHORIZED
             )
+        refresh = RefreshToken.for_user(user)
+        return Response({
+                'access_token': str(refresh.access_token),
+                'refresh_token': str(refresh),
+            }, status=status.HTTP_201_CREATED)
 
 class RefreshTokenAPIView(APIView):
     def post(self, request):
